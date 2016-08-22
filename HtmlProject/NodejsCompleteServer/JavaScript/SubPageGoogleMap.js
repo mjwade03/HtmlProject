b67e471a2d7872247866d1d2f9ea4aff24cf7108
mjwade03 <eason@mitake.com.tw>
2016-08-22 17:58:29 +0800

    1. 增加在額外資訊頁面所提供的資訊 2. 加入路徑規劃的功能

--- HtmlProject/NodejsCompleteServer/JavaScript/SubPageGoogleMap.js
+++ HtmlProject/NodejsCompleteServer/JavaScript/SubPageGoogleMap.js
@@ -1,7 +1,15 @@
-﻿var currentLng;
-var currentLat;
-var lastMarker;
+﻿var srcLng;
+var srcLat;
+
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var oldDirections = [];
+var currentDirections = null;
+var currentDirectionsTravelMode = google.maps.DirectionsTravelMode.DRIVING;
+var currentTargetPosition;
 function initSubPageMap(currentLat, currentLon) {
+    srcLng = currentLon;
+    srcLat = currentLat;
     geocoder = new google.maps.Geocoder();
     myMap = new google.maps.Map(document.getElementById('my_map'), {
         center: { lat: currentLat*1, lng: currentLon*1 },
@@ -10,12 +18,62 @@ function initSubPageMap(currentLat, currentLon) {
             position: google.maps.ControlPosition.LEFT_BOTTOM
         }
     });
+
+    directionsDisplay = new google.maps.DirectionsRenderer({
+        'map': myMap,
+        'preserveViewport': true,
+        'draggable': true
+    });
+
+    directionsDisplay.setPanel(document.getElementById("directions_panel"));
+
+    google.maps.event.addListener(directionsDisplay, 'directions_changed',
+        function () {
+            if (currentDirections) {
+                oldDirections.push(currentDirections);
+            }
+            currentDirections = directionsDisplay.getDirections();
+        });
+
     popup = new google.maps.InfoWindow();
 
     setSubPageMarkerWithTimeoutAndImage(currentLat * 1, currentLon * 1, "Your position", "Your position", 'Image/you-are-here.png', 100, 50, 50, true);
 
     //setSubPageMarkerWithTimeout(currentLat * 1, currentLon * 1, "Current position", 200);
 }
+
+function calcRoute(pFrom, pEnd, targetTravelMode) {
+
+    var start = pFrom;
+    var end = pEnd;
+    var request = {
+        origin: start,		//起始地
+        destination: end,	//目的地
+        travelMode: targetTravelMode //旅行工具 WALKING | DRIVING
+    };
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            document.getElementById('emptyResultDiv').style.visibility = 'collapse';
+            document.getElementById('directions_panel').style.visibility = 'visible';
+            //alert(directionsDisplay.getDirections().routes[0].legs[0].start_address);//起點地點：330台灣桃園縣桃園市興華路23號
+            //alert(directionsDisplay.getDirections().routes[0].legs[0].end_address);		//alert(directionsDisplay.getDirections().routes[0].legs[0].distance.text);//24.8公里
+            //alert(directionsDisplay.getDirections().routes[0].legs[0].duration.text);//31分鐘
+            //alert(directionsDisplay.getDirections().routes[0].copyrights);//地圖資料 2011 Kingway
+            //alert(directionsDisplay.getDirections().routes[0].legs[0].steps[0].instructions);//朝<b>西北</b>，走<b>興華路</b>，往<b>大智路</b>前進
+            //alert(directionsDisplay.getDirections().routes[0].legs[0].steps[0].distance.text);//0.3公里
+
+        }
+        else
+        {
+            document.getElementById('emptyResultDiv').style.visibility = 'visible';
+            document.getElementById('directions_panel').style.visibility = 'hidden';
+        }
+    });
+
+}
+
+
 function setSubPageMarkerWithTimeout(lat, lon, displayContent, timeout) {
 
     window.setTimeout(function () {
@@ -62,14 +120,39 @@ function setSubPageMarkerWithTimeoutAndImage(lat, lon, displayTitle, displayCont
         }
 
         var contentString = displayContent;
-
-        lastMarker = marker;
+        
         google.maps.event.addListener(marker, 'click', function () {
             popup.setContent(this.buborek);
             popup.open(myMap, this);
+            $("#DirectionMethodDiv").fadeIn(1000);
+
+            document.getElementById('drivingBtn').checked = true;                                   
+            currentTargetPosition = this.position;
+            calcRoute(new google.maps.LatLng(srcLat, srcLng), this.position, google.maps.DirectionsTravelMode.DRIVING);
 
         });
     }, timeout);
 
 
+}
+
+
+
+function onDirectiveTypeClick(sender) {
+    switch (sender.value)
+    {
+        case "DRIVING":
+            currentDirectionsTravelMode = google.maps.DirectionsTravelMode.DRIVING;
+            break;
+        case "BICYCLING":
+            currentDirectionsTravelMode = google.maps.DirectionsTravelMode.BICYCLING;
+            break;
+        case "TRANSIT":
+            currentDirectionsTravelMode = google.maps.DirectionsTravelMode.TRANSIT;
+            break;
+        case "WALKING":
+            currentDirectionsTravelMode = google.maps.DirectionsTravelMode.WALKING;
+            break;
+    }
+    calcRoute(new google.maps.LatLng(srcLat, srcLng), currentTargetPosition, currentDirectionsTravelMode);
 }
